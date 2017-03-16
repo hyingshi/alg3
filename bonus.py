6bd176fd9a3c48ad9afcd7e616ae9a794aeacd24
leian7 <leian@osuosl.org>
2017-03-16 14:28:17 -0700

    real clean + start bonus

---
+++ bonus.py
@@ -0,0 +1,47 @@
+###############################################################################
+# Yingshi Huang, Annie Lei
+# CS 325
+# Implementation 3
+# Extra credit problem: Local temperature change for Birmingham, Ala.
+###############################################################################
+
+import pulp
+import csv
+from math import pi, cos, sin
+
+season_const = 2 * pi / 365.25
+solar_const = season_const / 10.7
+
+tempChange = pulp.LpProblem("Birmingham Temperature Change", pulp.LpMinimize)
+
+M = pulp.LpVariable('M', 0)
+x0 = pulp.LpVariable('x0', 0)
+x1 = pulp.LpVariable('x1', 0)
+x2 = pulp.LpVariable('x2', 0)
+x3 = pulp.LpVariable('x3', 0)
+x4 = pulp.LpVariable('x4', 0)
+x5 = pulp.LpVariable('x5', 0)
+
+# Objective function
+tempChange += M, 'M'
+
+# Constraints:
+with open('Birmingham_clean.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';')
+    for row in reader:
+        if row[0] != 'STATION':             # skip header
+            T = float(row[7])
+            d = int(row[8])
+    
+            tempChange += x0 + x1*d + x2*cos(season_const*d) + x3*sin(season_const*d) + x4*cos(solar_const*d) + x5*sin(solar_const*d) - T <= M
+            tempChange += x0 + x1*d + x2*cos(season_const*d) + x3*sin(season_const*d) + x4*cos(solar_const*d) + x5*sin(solar_const*d) - T >= -M
+
+csvfile.close()
+
+# print(tempChange)
+tempChange.solve()                          # solve problem
+print(pulp.LpStatus[tempChange.status])     # was optimal found?
+
+# print the found values of x0, x1, ..., x5
+for var in tempChange.variables():
+    print("{} = {}".format(var.name, var.varValue))
--- clean_birmingham.py
+++ clean_birmingham.py
@@ -1,17 +1,36 @@
 import csv
+from datetime import date
 
-input_file = open('Birmingham.csv', 'r')
+input_file = open('Birmingham_unclean.csv', 'r')
 output_file = open('Birmingham_clean.csv', 'w')
 
 reader = csv.reader(input_file)
 writer = csv.writer(output_file)
 
-for row in reader:
-    if row[0] == 'STATION' or row[3] != '-9999':
-        writer.writerow(row)
-    else:
-        avg = (float(row[4]) + (float(row[5]))) / 2
-        writer.writerow([row[0], row[1], row[2], str(avg), row[4], row[5]])
+start = date(1960, 03, 15)
 
+for row in reader:
+    # write header
+    if row[0] == 'STATION':
+        writer.writerow(row)
+
+    # garbage row (TMIN TMAX are -9999):
+    elif row[4] == '-9999'and row[5] == '-9999':
+        pass
+
+    # normal row:
+    else:
+        # find days elapsed since 1960
+        end = date(int(row[2][:4]), int(row[2][4:6]), int(row[2][6:]))
+        day = (end - start).days
+
+        # if good TAVG, write row as-is with days elapsed
+        if row[3] != '-9999':
+            writer.writerow([row[0], row[1], row[2], row[3], row[4], row[5], str(day)])
+        # if bad TAVG, write row with good TAVG with days elapsed
+        else:
+            avg = (float(row[4]) + (float(row[5]))) / 2
+            writer.writerow([row[0], row[1], row[2], str(avg), row[4], row[5], str(day)])
+    
 input_file.close()
 output_file.close()
