162b7e09bd65b42d00ede93285a7fff3fb017dc4
leian7 <leian@osuosl.org>
2017-03-16 19:32:14 -0700

    fix TAVG to be avg of TMIN and TMAX for all rows'

--- clean_birmingham.py
+++ clean_birmingham.py
@@ -14,23 +14,19 @@ for row in reader:
     if row[0] == 'STATION':
         writer.writerow(row)
 
-    # garbage row (TMIN TMAX are -9999):
+    # garbage row (TMIN TMAX are -9999): don't write
     elif row[4] == '-9999'and row[5] == '-9999':
         pass
 
-    # normal row:
+    # normal row: write
     else:
         # find days elapsed since 1960
         end = date(int(row[2][:4]), int(row[2][4:6]), int(row[2][6:]))
         day = (end - start).days
 
-        # if good TAVG, write row as-is with days elapsed
-        if row[3] != '-9999':
-            writer.writerow([row[0], row[1], row[2], row[3], row[4], row[5], str(day)])
-        # if bad TAVG, write row with good TAVG with days elapsed
-        else:
-            avg = (float(row[4]) + (float(row[5]))) / 2
-            writer.writerow([row[0], row[1], row[2], str(avg), row[4], row[5], str(day)])
+        # write row with TAVG = (TMIN + TMAX) / 2, with days elapsed
+        avg = (float(row[4]) + (float(row[5]))) / 2
+        writer.writerow([row[0], row[1], row[2], str(avg), row[4], row[5], str(day)])
     
 input_file.close()
 output_file.close()
